2803347220b7bae5bff67e7c6f4041fffd6a0b11
Unknown <florentin.paillier@gmail.com>
2017-11-24 12:20:54 +0100

    pom

--- src/test/java/JoueurTest.java
+++ src/test/java/JoueurTest.java
@@ -1,9 +1,7 @@
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 
 import exception.ExceptionPlacerPierre;
 
@@ -11,36 +9,35 @@ public class JoueurTest {
 
 	@Test
 	public void testJoueur() {
-	//	fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
-	public void testPlacerPierre()  {
-	
-			Plateau p = new Plateau (10, 10);
-			Joueur blanc = new Joueur("Blanc", p, "Flo");
-			try {
-				blanc.placerPierre(19, 19);
-				fail("manque exception");
-			} catch (ExceptionPlacerPierre e) {
-							
-				
-			}
-			
+	public void testPlacerPierre() {
+
+		Plateau p = new Plateau(10, 10);
+		Joueur blanc = new Joueur("Blanc", p, "Flo");
+		try {
+			blanc.placerPierre(19, 19);
+			fail("manque exception");
+		} catch (ExceptionPlacerPierre e) {
+
+		}
+
 		try {
 			blanc.placerPierre(0, 0);
 			blanc.placerPierre(0, 0);
-			
+
 			fail("manque execption");
 		} catch (ExceptionPlacerPierre e) {
-			
+
 		}
-			
-	}	
+
+	}
 
 	@Test
 	public void testPasserTour() {
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
